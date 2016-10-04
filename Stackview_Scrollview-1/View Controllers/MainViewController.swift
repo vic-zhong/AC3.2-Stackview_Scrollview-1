af05719823777b2f40197682f6a8755eaf48be25
Louis Tur <louis.tur@gmail.com>
2016-10-04 03:42:59 -0400

    ready for lesson

--- Stackview_Scrollview-1/View Controllers/MainViewController.swift
+++ Stackview_Scrollview-1/View Controllers/MainViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var blueView: UIView!
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +20,6 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    
-    print("Content Size: \(self.scrollView.contentSize)")
-    print("ScrollView Bounds: \(self.scrollView.bounds)")
-    print("ScrollView Frame: \(self.scrollView.frame)")
-    
-    print("Blue View Frame: \(self.blueView.frame)")
-    print("Blue View Bounds: \(self.blueView.bounds)")
-    
-    print("View Frame: \(self.view.frame)")
-    print("View Bounds: \(self.view.bounds)")
-  }
 
     /*
     // MARK: - Navigation
