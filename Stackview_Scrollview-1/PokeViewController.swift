594db5f8b494716b04b574fc1f99c84ba16e63b1
Victor Zhong <viczhong@gmail.com>
2016-10-05 16:03:18 -0400

    Sigh

--- Stackview_Scrollview-1/PokeViewController.swift
+++ Stackview_Scrollview-1/PokeViewController.swift
@@ -1,38 +1,26 @@
 //
-//  MainViewController.swift
+//  PokeViewController.swift
 //  Stackview_Scrollview-1
 //
-//  Created by Louis Tur on 10/2/16.
+//  Created by Victor Zhong on 10/5/16.
 //  Copyright © 2016 C4Q. All rights reserved.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
-  
-	@IBOutlet weak var scrollView: UIScrollView!
-	
+class PokeViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		print("Content Size: \(self.scrollView.contentSize)")
-		print("ScrollView Bounds: \(self.scrollView.bounds)")
-		print("ScrollView Frame: \(self.scrollView.frame)")
-		
-		print("View Frame: \(self.view.frame)")
-		print("View Bounds: \(self.view.bounds)")
-	}
-	
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     /*
     // MARK: - Navigation
